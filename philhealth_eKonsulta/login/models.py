c10861077e96ee5a0bfce9ce8b6f9c1e2ade0245
Rhod Celister Sol <rhodcelistersol@gmail.com>
2025-12-06 14:01:44 +0800

    Patch 0.0.1_1
    - added the template and statis folder

--- philhealth_eKonsulta/login/models.py
+++ philhealth_eKonsulta/login/models.py
@@ -9,5 +9,5 @@ class CustomUser(AbstractUser):
         ('FINANCE', 'Finance'),
     ]
     role = models.CharField(max_length = 20, choices = ROLE_CHOICES, default = 'PATIENT')
-
+    
 # Create your models here.
