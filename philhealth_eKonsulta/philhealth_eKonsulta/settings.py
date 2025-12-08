145407141dfd50b3126cc01f93399eb18ae09925
Kerneil Rommel Gocotano <gocotanokerlemmor@gmail.com>
2025-12-09 00:01:28 +0800

    Patch 0.0.1_3
    [+] Added Script for Docker Script
    [+] Change Configuration for Setting.py ( Docker Mode )

--- philhealth_eKonsulta/philhealth_eKonsulta/settings.py
+++ philhealth_eKonsulta/philhealth_eKonsulta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t6l*-(@i&a9c-7@sp_4j*(e%^+6757&w&_z7wcx6x97re&()$x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # Set to Enable when using Docker, yeahhh.... My Styleee
-DOCKER_MODE = False
+DOCKER_MODE = True
 
 ALLOWED_HOSTS = []
 
@@ -84,10 +84,10 @@ if DOCKER_MODE:
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'eKonsulta_db',
-            'USER': 'postgres',
-            'PASSWORD': 'password1',
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'USER': 'admin',
+            'PASSWORD': 'admin123',
+            'HOST': 'localhost',   # Django is outside Docker → use localhost
+            'PORT': '9095',        # Port forwarded from Docker to host
         }
     }
 else:
